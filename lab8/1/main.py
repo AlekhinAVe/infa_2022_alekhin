0e8471cb31b2e613c69b27e66c9bb1c0fbd85f48
Alekhin Andrey <aalekhin2004@yandex.ru>
2022-11-01 11:09:16 +0300

    1st commit

--- lab8/1/main.py
+++ lab8/1/main.py
@@ -5,19 +5,32 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
+screen.fill((255, 255, 255))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+circle(screen, (255, 255, 0), (200, 175), 100)
+circle(screen, (0, 0, 0), (200, 175), 100, 5)
+
+circle(screen, (255, 0, 0), (235, 150), 20)
+circle(screen, (0, 0, 0), (235, 150), 20, 5)
+circle(screen, (0, 0, 0), (235, 150), 10)
+
+circle(screen, (255, 0, 0), (165, 150), 20)
+circle(screen, (0, 0, 0), (165, 150), 20, 5)
+circle(screen, (0, 0, 0), (165, 150), 10)
+
+rect(screen, (0, 0, 0), (150, 205, 100, 15))
+
+polygon(screen, (0, 0, 0), [(115,155), (110,150),
+                               (175,100), (180,105)])
+
+polygon(screen, (0, 0, 0), [(285,155), (290,150),
+                               (225,100), (220,105)])
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+
+screen.fill((255, 255, 0))
 
 while not finished:
     clock.tick(FPS)
